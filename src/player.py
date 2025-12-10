a123c76e92c54f5f7aef0ee2b9621b717d105668
Hondrocea Dragoș <hondrocea.dragos01@gmail.com>
2025-12-10 23:12:26 +0200

    New player model integrated - animation TBA

--- src/player.py
+++ src/player.py
@@ -12,10 +12,30 @@ import pygame
 ### Notes ###
 # 'draw' method could have a return type (error checking)
 
+# Helper function - will add animations later
+def loadPlayerSprites(radius: float):
+        sprites = {}
+
+        idleSprite = pygame.image.load("../assets/sprites/playerSprites/idleSprite.png").convert_alpha()
+        idleSprite = pygame.transform.smoothscale(idleSprite, (int(2 * radius), int(2 * radius)))
+
+        flySprite = pygame.image.load("../assets/sprites/playerSprites/flySprite.png").convert_alpha()
+        flySprite = pygame.transform.smoothscale(flySprite, (int(2 * radius), int(2 * radius)))
+
+        dropSprite = pygame.image.load("../assets/sprites/playerSprites/dropSprite.png").convert_alpha()
+        dropSprite = pygame.transform.smoothscale(dropSprite, (int(2 * radius), int(2 * radius)))
+
+        sprites["idleSprite"] = idleSprite
+        sprites["flySprite"] = flySprite
+        sprites["dropSprite"] = dropSprite
+
+        return sprites
+
 # 'Player' class declaration and definition
 class Player:
     # Fields specifically declared for access modifiers (private)
     _radius: float = 3.0
+    _currentSprite: str = None
     def __init__(self, screen: pygame.Surface,
                  currPos: pygame.Vector2 = None,
                  radius: float = 3.0,
@@ -24,6 +44,14 @@ class Player:
         self.screen = screen
         self._radius = radius
         self.velocity = velocity
+
+        # Load sprites
+        self.sprites = loadPlayerSprites(self._radius)
+        self._currentSprite = self.sprites["idleSprite"]
+
+        # Default state
+        self.jumpPressed = False
+        self.state = "IDLE"
 
         # Spawn position
         if currPos is None:
@@ -34,17 +62,47 @@ class Player:
         else:
             self.currPos = currPos
 
-        # Sprite loading
-        self.image = pygame.image.load("../assets/sprites/player.png").convert_alpha()
-        self.image = pygame.transform.smoothscale(self.image,
-                                                  (int(radius*2),
-                                                   int(radius*2)))
+    # Change sprites
+    def changeSprite(self, name: str) -> None:
+         self._currentSprite = self.sprites[name]
 
+    # Core of animation engine
+    def decideState(self) -> None:
+        # Player is on ground
+        if self.currPos.y + self._radius >= self.screen.get_height():
+            self.state = "IDLE"
+        else:
+            # Player is flying
+            if self.velocity.y < 0:
+                self.state = "FLYING"
+            # VERY IMPORTANT FOR GAME FEEL - if 'jump' is pressed
+            # the flying sprite is shown - makes it feel like the bird
+            # is trying to fly harder
+            elif self.jumpPressed and self.currPos.y + self._radius < self.screen.get_height():
+                self.state = "FLYING"
+            # Player is dropping
+            elif self.velocity.y > 0:
+                self.state = "FALLING"
+
+    # Animation engine - in-between frames TBA
+    def animatePlayer(self) -> None:
+        match self.state:
+            case "IDLE":
+                self.changeSprite("idleSprite")
+
+            case "FLYING":
+                self.changeSprite("flySprite")
+
+            case "FALLING":
+                self.changeSprite("dropSprite")
+                
     # Display method
     def draw(self) -> None:
         # Draw only the player - no hitbox
-        rect = self.image.get_rect(center=self.currPos)
-        self.screen.blit(self.image, rect)
+        rect = self._currentSprite.get_rect(center = self.currPos)
+        self.screen.blit(self._currentSprite, rect)
+        
+        # Player hitbox
         pygame.draw.circle(self.screen,
                            "red",
                            self.currPos,
